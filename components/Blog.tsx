7ded75f716942660d064dbbe117d2669f665045b
divyanshu25 <divyanshu.g25@gmail.com>
2026-03-02 09:31:41 -0800

    remove animate from Button
    Signed-off-by: divyanshu25 <divyanshu.g25@gmail.com>

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -78,9 +78,9 @@ export default function Blog() {
                   </div>
                   
                   {/* Read more link */}
-                  <div className="flex items-center text-blue-600 font-bold group-hover:gap-2 transition-all pt-3 border-t border-gray-100">
+                  <div className="flex items-center text-blue-600 font-bold pt-3 border-t border-gray-100">
                     <span>Read Article</span>
-                    <svg className="w-5 h-5 ml-1 group-hover:translate-x-1 transition-transform" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <svg className="w-5 h-5 ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
                     </svg>
                   </div>
